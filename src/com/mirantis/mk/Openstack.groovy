7889a54be85004b87164301ebfebbd4e556a1108
ohryhorov <golantster@gmail.com>
2017-09-04 13:46:36 +0300

    Update Openstack.groovy

--- src/com/mirantis/mk/Openstack.groovy
+++ src/com/mirantis/mk/Openstack.groovy
@@ -142,7 +142,7 @@ def createHeatEnv(file, environment = [], original_file = null) {
 
     p = entries(environment)
     for (int i = 0; i < p.size(); i++) {
-        def value=p.get(i)[1].replaceAll("\n", '\\n')
+        def value=p.get(i)[1].replaceAll("\n", '\\\\n')
         envString = "${envString}  ${p.get(i)[0]}: \"${value}\"\n"
     }
 
